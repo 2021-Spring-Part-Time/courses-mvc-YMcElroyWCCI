d8203d316fc05bb36297daf571a923acf11b6633
YMcElroyWCCI <yuli.mcelroy@gmail.com>
2021-05-30 01:14:40 -0400

    first commit

---
+++ src/main/java/org/wecancodeit/courses/CourseRepository.java
@@ -0,0 +1,39 @@
+package org.wecancodeit.courses;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class CourseRepository {
+
+    Map<Long, Course> coursesList = new HashMap<>();
+
+    //constructor to handle our database
+    public CourseRepository(){
+        Course reading = new Course(1L, "Reading", "I love J.R.R. Tolkien, learn all about this author in this course.");
+        Course music = new Course(2L, "Music 101", "School of Rock!");
+        Course java = new Course (3L, "Java with Spring", "The best course ever!");
+
+        coursesList.put(reading.getId(), reading);
+        coursesList.put(music.getId(), music);
+        coursesList.put(java.getId(), java);
+    }
+
+    //constructor for testing purposes only...uses varArgs to add none or as many objects as we want
+    public CourseRepository(Course ...coursesToAdd) {
+        for(Course course: coursesToAdd){
+            coursesList.put(course.getId(), course);
+        }
+    }
+
+    public Course findOne(long id) {
+        return coursesList.get(id);
+    }
+
+    public Collection<Course> findAll() {
+        return coursesList.values();
+    }
+}
